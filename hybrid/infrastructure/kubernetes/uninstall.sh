d3ace1ed956d4d5d5ff72b1120bec0064bd947d8
Zhen Ju <juzhen@huawei.com>
2018-09-15 10:19:20 +0800

    Polish the scripts.

--- hybrid/infrastructure/kubernetes/uninstall.sh
+++ hybrid/infrastructure/kubernetes/uninstall.sh
@@ -5,5 +5,5 @@ set -e
 
 SCRIPT_PATH=$(cd $(dirname $0);pwd)
 
-kubectl delete -f ${SCRIPT_PATH}//servicecomb.yaml
+kubectl delete -f ${SCRIPT_PATH}/servicecomb.yaml
 kubectl delete -f ${SCRIPT_PATH}/namespace.yaml
